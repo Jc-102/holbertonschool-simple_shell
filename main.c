843e9da3f4b6198da45542021342d02e4ad5a39f
Jc07 <jeanc1725@gmail.com>
2026-09-24 17:58:20 -0400

    shell

---
+++ main.c
@@ -0,0 +1,62 @@
+#include "shell.h"
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+/**
+ * main - entry point for the simple shell
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char *line;
+	size_t len;
+	ssize_t read;
+	pid_t child_pid;
+	int status;
+
+	line = NULL;
+	len = 0;
+
+	while (1)
+	{
+		printf("($) ");
+
+		read = getline(&line, &len, stdin);
+		if (read == -1)
+		{
+			printf("\n");
+			free(line);
+			exit(0);
+		}
+
+		if (line[read - 1] == '\n')
+			line[read - 1] = '\0';
+
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("Error");
+			continue;
+		}
+
+		if (child_pid == 0)
+		{
+			if (execve(line, &line, environ) == -1)
+			{
+				perror("Error");
+				exit(1);
+			}
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
+
+	return (0);
+}
